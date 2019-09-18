1026837979b5b32e1578b813aa5cf713113dbc52
Jen E <jen@wecancodeit.org>
2019-09-18 08:33:53 -0400

    updates to car tests

--- cs-car-garage.Tests/CarTests.cs
+++ cs-car-garage.Tests/CarTests.cs
@@ -5,22 +5,40 @@ namespace cs_car_garage.Tests
 {
     public class CarTests
     {
+        Car car;
+
+        public CarTests()
+        {
+            car = new Car();
+        }
+
         [Fact]
         public void shouldAccelerate()
         {
-            // Tests Accelerate() method in Car increases Speed
+            // Tests Accelerate() method in Car increases Speed by 10
             // Arrange
+            // Car car = new Car();
+            car.Speed = 0;
+
             // Act
+            car.Accelerate();
+
             // Assert
+            Assert.Equal(10, car.Speed);
         }
 
         [Fact]
         public void shouldConsumeFuel()
         {
-            // Tests Accelerate() method reduces Fuel amount
+            // Tests Accelerate() method reduces Fuel amount by 2
             // Arrange
+            car.FuelLevel = 100;
+
             // Act
+            car.Accelerate();
+
             // Assert
+            Assert.Equal(98, car.FuelLevel);
         }
 
         [Fact]
@@ -28,8 +46,13 @@ namespace cs_car_garage.Tests
         {
             // Tests AddFuel() method increases Fuel amount
             // Arrange
+            car.FuelLevel = 0;
+
             // Act
+            car.AddFuel();
+
             // Assert
+            Assert.Equal(20, car.FuelLevel);
         }
 
         [Fact]
