634a2b2921a6280c1ee0e244cc1088741118d42e
Jen E <jen@wecancodeit.org>
2019-09-18 08:30:53 -0400

    added empty test classes

---
+++ cs-car-garage.Tests/CarTests.cs
@@ -0,0 +1,62 @@
+using System;
+using Xunit;
+
+namespace cs_car_garage.Tests
+{
+    public class CarTests
+    {
+        [Fact]
+        public void shouldAccelerate()
+        {
+            // Tests Accelerate() method in Car increases Speed
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        [Fact]
+        public void shouldConsumeFuel()
+        {
+            // Tests Accelerate() method reduces Fuel amount
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        [Fact]
+        public void shouldRefuel()
+        {
+            // Tests AddFuel() method increases Fuel amount
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        [Fact]
+        public void shouldSlowDown()
+        {
+            // Tests Brake() method in Car reduces Speed amount
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        [Fact]
+        public void shouldStart()
+        {
+            // Tests ToggleEngine() method starts car when its off
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        [Fact]
+        public void shouldTurnOff()
+        {
+            // Tests ToggleEngine() method turns car off when its on
+            // Arrange
+            // Act
+            // Assert
+        }
+    }
+}
---
+++ cs-car-garage.Tests/GarageTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace cs_car_garage.Tests
+{
+    public class GarageTests
+    {
+        [Fact]
+        public void shouldAddCarToGarage()
+        {
+            // Tests AddCar() method
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        [Fact]
+        public void shouldRemoveCarFromGarage()
+        {
+            // Tests RemoveCar() method
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        [Fact]
+        public void shouldFuelAllCars()
+        {
+            // Tests FuelAllCars() method
+            // Arrange
+            // Act
+            // Assert
+        }
+
+        [Fact]
+        public void shouldTestDriveOneCar()
+        {
+            // Use the Program class to let you select a single car
+            // Program class should then let you choose what you want to do with that car
+            // You do not need to test user input in the Program class
+        }
+
+        [Fact]
+        public void youShouldNameThisTest()
+        {
+            // Should be able to list stats of all cars
+            // Create necessary method(s)
+            // Garage class should provide cars
+            // Program class should list all stats
+        }
+    }
+}
