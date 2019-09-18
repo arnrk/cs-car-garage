b5ba2566111f2817119befc61caf07ee3383304c
Jen E <jen@wecancodeit.org>
2019-09-18 08:34:30 -0400

    updates to garage tests

--- cs-car-garage.Tests/GarageTests.cs
+++ cs-car-garage.Tests/GarageTests.cs
@@ -7,13 +7,26 @@ namespace cs_car_garage.Tests
 {
     public class GarageTests
     {
+        Garage garage;
+
+        public GarageTests()
+        {
+            garage = new Garage();
+        }
+
         [Fact]
         public void shouldAddCarToGarage()
         {
             // Tests AddCar() method
             // Arrange
+            garage.Cars.Clear();
+
             // Act
+            garage.AddCar("Ford", "Explorer");
+
             // Assert
+            Assert.Single(garage.Cars);
+
         }
 
         [Fact]
@@ -21,8 +34,15 @@ namespace cs_car_garage.Tests
         {
             // Tests RemoveCar() method
             // Arrange
+            Car addedCar = new Car("Ford", "Explorer");
+            garage.Cars.Add(addedCar);
+            int carIndex = garage.Cars.IndexOf(addedCar);
+
             // Act
+            garage.RemoveCar(carIndex);
+
             // Assert
+            Assert.Empty(garage.Cars);
         }
 
         [Fact]
@@ -30,8 +50,23 @@ namespace cs_car_garage.Tests
         {
             // Tests FuelAllCars() method
             // Arrange
+            Car addedCar1 = new Car("Ford", "Explorer");
+            addedCar1.FuelLevel = 0;
+            garage.Cars.Add(addedCar1);
+
+            Car addedCar2 = new Car("Nissan", "Sentra");
+            garage.Cars.Add(addedCar2);
+            addedCar2.FuelLevel = 0;
+
             // Act
+            garage.FuelAllCars();
+
             // Assert
+            //Assert.Equal(20, addedCar1.FuelLevel);
+            //Assert.Equal(20, addedCar2.FuelLevel);
+
+            Assert.True(garage.Cars.TrueForAll(car => car.FuelLevel == 20));
+
         }
 
         [Fact]
