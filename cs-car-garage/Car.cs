43fd8553cbe199d4e6d2e47feb5e54ea9b4ab603
Jen E <jen@wecancodeit.org>
2019-09-18 08:35:58 -0400

    code added to car class

--- cs-car-garage/Car.cs
+++ cs-car-garage/Car.cs
@@ -4,7 +4,36 @@ using System.Text;
 
 namespace cs_car_garage
 {
-    class Car
+    public class Car
     {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Speed { get; set; }
+        public int FuelLevel { get; set; }
+
+        public Car()
+        {
+
+        }
+
+        public Car(string make, string model)
+        {
+            Make = make;
+            Model = model;
+            Speed = 0;
+            FuelLevel = 100;
+        }
+
+        public void Accelerate()
+        {
+            // Speed = Speed + 10;
+            Speed += 10;
+            FuelLevel -= 2;
+        }
+
+        public void AddFuel()
+        {
+            FuelLevel += 20;
+        }
     }
 }
