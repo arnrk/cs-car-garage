f2422f69bd54a75c22c1e3d9ce4ccfc7b25f3097
Jen E <jen@wecancodeit.org>
2019-09-18 08:36:18 -0400

    code added to garage class

--- cs-car-garage/Garage.cs
+++ cs-car-garage/Garage.cs
@@ -4,7 +4,27 @@ using System.Text;
 
 namespace cs_car_garage
 {
-    class Garage
+    public class Garage
     {
+        public List<Car> Cars = new List<Car>();
+
+        public void AddCar(string make, string model)
+        {
+            Car car = new Car(make, model);
+            Cars.Add(car);
+        }
+
+        public void RemoveCar(int index)
+        {
+            Cars.RemoveAt(index);
+        }
+
+        public void FuelAllCars()
+        {
+            foreach (Car car in Cars)
+            {
+                car.AddFuel();
+            }
+        }
     }
 }
